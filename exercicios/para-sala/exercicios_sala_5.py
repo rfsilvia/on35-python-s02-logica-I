c80717f2649b6770f0c6f4bd6d35ea9c46a2777b
Silvia RF <silvia.follador@gmail.com>
2024-06-18 11:47:13 -0400

    add exercicio 5

---
+++ exercicios/para-sala/exercicios_sala_5.py
@@ -0,0 +1,9 @@
+# Faça um programa que converta metros para centímetros
+
+medida_em_metros = float(input("informe a medida em metros:"))
+print(medida_em_metros)
+
+conversao_centimetros = (medida_em_metros * 100)
+
+print(f"A conversão de {medida_em_metros} m para centímetros é de {conversao_centimetros} cm.")
+
